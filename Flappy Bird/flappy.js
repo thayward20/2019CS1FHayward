36c3cc17b6ddb48cb5db90b7c8bd4bbe4b1581da
thayward20 <thayward20@nscds.org>
2019-12-18 19:15:36 +0000

    New flappy

--- Flappy Bird/flappy.js
+++ Flappy Bird/flappy.js
@@ -1,109 +1,63 @@
 var myGamePiece;
-var myObstacles = [];
- 
+
 function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);   
-     myGamePiece.gravity = 0.05;
+    myGamePiece = new component(30, 30, "red", 80, 75);
     myGameArea.start();
 }
- 
+
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
-         window.addEventListener('keydown', function (e) {
-      myGameArea.key = e.keyCode;
-    })
-    window.addEventListener('keyup', function (e) {
-      myGameArea.key = false;
-    })
-        },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);       
     },
     stop : function() {
         clearInterval(this.interval);
+    },    
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
- 
 function component(width, height, color, x, y, type) {
-    this.type= type;
+    this.type = type;
     this.width = width;
     this.height = height;
+    this.x = x;
+    this.y = y;    
     this.speedX = 0;
     this.speedY = 0;    
     this.gravity = 0.05;
     this.gravitySpeed = 0;
-    this.x = x;
-    this.y = y;    
-        this.update = function() {
+    this.update = function() {
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-        this.newPos = function() {
+    }
+    this.newPos = function() {
         this.gravitySpeed += this.gravity;
         this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;        
-           this.hitBottom();
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
     }
     this.hitBottom = function() {
         var rockbottom = myGameArea.canvas.height - this.height;
         if (this.y > rockbottom) {
             this.y = rockbottom;
+            this.gravitySpeed = 0;
         }
-    }        
-    }  
-   this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-            crash = false;
-        }
-        return crash;
-    }    
+    }
+}
+
 function updateGameArea() {
-    var x, y;
-    for (i = 0; i < myObstacles.length; i += 1) {
-        if (myGamePiece.crashWith(myObstacles[i])) {
-            myGameArea.stop();
-            return;
-        } 
-    }
     myGameArea.clear();
-    myGameArea.frameNo += 1;
-    if (myGameArea.frameNo == 1 || everyinterval(150)) {
-        x = myGameArea.canvas.width;
-        y = myGameArea.canvas.height - 200;
-        myObstacles.push(new component(10, 200, "green", x, y));
-    }
-    for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -1;
-        myObstacles[i].update();
-    }
-    myGamePiece.newPos();    
+    myGamePiece.newPos();
     myGamePiece.update();
 }
 
-function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
-    return false;
-}  
-       function accelerate(n) {
-    if (!myGameArea.interval) {myGameArea.interval = setInterval(updateGameArea, 20);}
-
+function accelerate(n) {
     myGamePiece.gravity = n;
+<button onmousedown="accelerate(-0.2)" onmouseup="accelerate(0.05)">ACCELERATE</button> 
 }
