8fc0ad738aaa278b83ab792ef4f039afd2dd758e
thayward20 <thayward20@nscds.org>
2020-02-10 15:25:25 +0000

    ne code

--- New Project/kobe.js
+++ New Project/kobe.js
@@ -1,12 +1,11 @@
-$('baseballmap').click(
+
+$('#box1').click(
     function(){
-        $('#Rockies').fadeIn();
+        $('[stadium]').fadeOut();
+        $('#Coors').fadeIn();
     }
 );
 
-$( "div" ).mousemove(function( event ) {
-  var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
-  var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
-  $( "span" ).first().text( "( event.pageX, event.pageY ) : " + pageCoords );
-  $( "span" ).last().text( "( event.clientX, event.clientY ) : " + clientCoords );
-});
+
+
+
