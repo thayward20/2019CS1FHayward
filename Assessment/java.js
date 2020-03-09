163a019b334e8419cdf73e9f3d254a8ae530c73e
thayward20 <thayward20@nscds.org>
2020-03-09 18:18:07 +0000

    assessment

---
+++ Assessment/java.js
@@ -0,0 +1,37 @@
+
+  $("#box1").click(function(){
+    $("#bar").css("background-color", "orange");
+    $("#bar").text("1")
+    $("#box6").text(Math.floor(Math.random()*100)+1)
+}
+  );
+
+  $("#box2").click(function(){
+    $("#bar").css("background-color", "blue");
+  $("#bar").text("2")
+}
+  );
+
+  $("#box3").click(function(){
+    $("#bar").css("background-color", "maroon");
+  $("#bar").text("3")
+}
+  );
+
+  $("#box4").click(function(){
+    $("#bar").css("background-color", "maroon");
+  $("#bar").text("4")
+
+}
+  );
+
+  $("#box5").click(function(){
+    $("#bar").css("background-color", "blue");
+   $("#bar").text("5")
+}
+  );
+
+   $("#box6").click(function(){
+    $("#bar").css("background-color", "orange");
+  }
+  );
